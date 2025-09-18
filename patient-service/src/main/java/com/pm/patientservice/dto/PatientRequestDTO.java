bdabee055d5c58ea61a5320cb60221c85994cf47
nagarajukushal <kushalnagaraj1997@gmail.com>
2025-09-18 12:22:27 -0500

    Patient PUT and DELETE Working
    Patient update and delete end points working 2:39:00

--- patient-service/src/main/java/com/pm/patientservice/dto/PatientRequestDTO.java
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientRequestDTO.java
@@ -1,5 +1,6 @@
 package com.pm.patientservice.dto;
 
+import com.pm.patientservice.dto.validators.CreatePatientValidationGroup;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -23,7 +24,7 @@ public class PatientRequestDTO {
     @NotBlank(message = "Date of birth is required")
     private String dateOfBirth;
 
-    @NotBlank( message =
+    @NotBlank(groups = CreatePatientValidationGroup.class, message =
             "Registered date is required")
     private String registeredDate;
 
